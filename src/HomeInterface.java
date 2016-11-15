743541976e42c09d25871c8f6e5a502430151c29
pofferfish <malin.lundberg91@gmail.com>
2016-11-15 09:46:25 +0100

    Home interface

---
+++ src/HomeInterface.java
@@ -0,0 +1,7 @@
+import javax.ejb.*;
+import java.rmi.RemoteException;
+
+/*Factory for bean instances. Provides instance to client*/
+public interface HomeInterface extends EJBHome {
+    public HelloBean create() throws CreateException, RemoteException;
+}

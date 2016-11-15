e02e86f15cb3ac1df384c15409939ee4cff59f0b
pofferfish <malin.lundberg91@gmail.com>
2016-11-15 09:59:38 +0100

    Bean implements session bean

--- src/HelloBean.java
+++ src/HelloBean.java
@@ -1,5 +1,25 @@
 import javax.ejb.*;
 
-public class HelloBean {
+public class HelloBean implements SessionBean {
 
+
+    @Override
+    public void setSessionContext(SessionContext sessionContext) {
+        System.out.println("session context");
+    }
+
+    @Override
+    public void ejbRemove() {
+        System.out.println("ejb remove");
+    }
+
+    @Override
+    public void ejbActivate() {
+        System.out.println("ejb activate");
+    }
+
+    @Override
+    public void ejbPassivate() {
+        System.out.println("ejb passivate");
+    }
 }
